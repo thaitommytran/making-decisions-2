a5a9b5e5b904cb0891f299f0bbd7d96052c4efd3
Thai Tran <thai.tommy.tran@gmail.com>
2022-05-05 14:46:00 -0500

    beginner problems

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -15,6 +15,8 @@ let firstItem = arr[0];
 */
 
 //Code Here
+let lastItemRemoved = arr.pop();
+console.log(arr);
 
 ////////// PROBLEM 3 //////////
 
@@ -27,7 +29,9 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 */
 
 //Code Here
-
+for (let i = 0; i < family.length; i++) {
+  console.log(family[i]);
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -42,7 +46,11 @@ let evensArr = []
 */
 
 //Code Here
-
+for (let i = 0; i < nums.length; i++) {
+  if (nums[i] % 2 === 0) {
+    evensArr.push(nums[i]);
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -52,11 +60,21 @@ var score = 74
 // Do not edit the code above.
 
 /*
-  Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
+  Determine the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
 */
 
 //Code Here
-
+if (score >= 90) {
+  console.log('A');
+} else if (score >= 80) {
+  console.log('B');
+} else if (score >= 70) {
+  console.log('C');
+} else if (score >= 60) {
+  console.log('D');
+} else {
+  console.log('F');
+}
 
 ////////// Intermediate Problems //////////
 
